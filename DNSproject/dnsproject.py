745958616defb2b5373249a9e33a0515c0647274
luvpreetkaur <54563105+luvpreetkaur@users.noreply.github.com>
2019-10-22 12:32:13 -0400

    Final code (eliminated false positives)

--- DNSproject/dnsproject.py
+++ DNSproject/dnsproject.py
@@ -9,14 +9,16 @@ with open('report.txt', 'w') as report:  # blank file
     report.write("")
 
 #Regex
+# ^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$
 domainRegex = re.compile(r'''([0-9a-z-]+\.[a-z-]+\.?[a-z-]+\.?[a-z-]+\.?\s[A-Z]+)''', re.VERBOSE)  # regex domain name
-lineRegex = re.compile(r'''(([0-9][0-9][0-9][0-9])-([0-9]?[0-9])-([0-9]?[0-9]) \s   # regex time
+lineRegex = re.compile(r'''(([0-9][0-9][0-9][0-9])-([0-9]?[0-9])-([0-9]?[0-9]) \s   
                                 (0?0|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9]).(([0-9][0-9])?[0-9]))''',
-                       re.VERBOSE)
+                       re.VERBOSE)	# regex time
 t = []
-report = os.path.normpath('dnslog.txt')
-with open('dnslog.txt', 'r') as in_file:  # input file
+report = os.path.normpath('dnslog1.txt')
+with open('dnslog1.txt', 'r') as in_file:  # input file
     line = in_file.read()  # read file
+    # line=line.replace('A','') # tried to segregate ipv4 and ipv6
     d = domainRegex.findall(line)
     d = [rm[: -3] for rm in d]
     e = lineRegex.findall(line)
@@ -38,17 +40,25 @@ while j < len(key):
     diff = (end_dt - start_dt)
     key1_len = len(key1)
     value1_len = len(value1)
-    if diff.seconds >= 60:  # checking for the sites opened within 60-90 seconds
+    if diff.seconds >= 62:  # checking for the sites opened within 60-90 seconds
         with open('report.txt', 'a+') as report:
             report.write('\n')
             report.write('{}: {} Time:{}'.format(value[0], key1_len, key[0]) + '\n')  # write main domain names
-            for j, each in enumerate(value1, start=1):
-                report.write("{}.{}".format(str(j), str(each)) + '\n')  # write sub domain names
+            for j, sub in enumerate(value1, start=1):
+                report.write('{}.{}'.format(str(j), str(sub)) + '\n')  # write sub domain names
         del key[0:key1_len + 1]  # deleting each key and value after writing in file
         del value[0:value1_len + 1]
         del key1[:]
         del value1[:]
-    elif diff.seconds < 60:
+        j = 0
+    elif diff.seconds < 62:
         key1.append(key[j])
         value1.append(value[j])
     j = j + 1
+
+with open('report.txt', 'a+') as report:    #to reiterate the list again and write remaining sites (to eliminate false positives)
+    key1_len = len(key1)
+    report.write('\n')
+    report.write('{}: {} Time:{}'.format(value[0], key1_len, key[0]) + '\n')
+    for j, sub in enumerate(value1, start=1):
+        report.write('{}.{}'.format(str(j), str(sub)) + '\n')
